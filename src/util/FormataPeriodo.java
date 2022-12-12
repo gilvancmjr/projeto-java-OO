2c417d8d60bc19da457d84a9bc1e1a8e1746912e
Gilvan Júnior <juniormonteiroti@gmail.com>
2022-12-12 15:03:28 -0300

    first commit

--- src/util/FormataPeriodo.java
+++ src/util/FormataPeriodo.java
@@ -5,10 +5,12 @@ public abstract class FormataPeriodo {
 	public static int formataPeriodoEmMes(String Periodo) {
 		int dia = Integer.parseInt(Periodo.substring(0, 2));
 		return dia;
+		
 	}
 
 	public static int formataPeriodoEmAno(String Periodo) {
 		int ano = Integer.parseInt(Periodo.substring(3, 7));
 		return ano;
+		
 	}
 }
